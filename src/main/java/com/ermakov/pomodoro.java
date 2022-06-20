18f8418299609fecdfc5a21da62470cdb8c8e3f8
Alexander <sash.taichi@gmail.com>
2022-06-20 22:24:58 +0300

    fix not2

--- src/main/java/com/ermakov/pomodoro.java
+++ src/main/java/com/ermakov/pomodoro.java
@@ -4,6 +4,6 @@ public class pomodoro {
     public static void main(String[] args) {
         System.out.println("Hello pomodoro");
 
-        внокеокегокеококо
+        frhrthhrjhrjrtjgit
     }
 }
